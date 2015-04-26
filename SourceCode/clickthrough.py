b527586e684c401c569919405af08ccde045536e
Siddhartha Singh Sandhu <sandhus@usc.edu>
2015-04-25 22:10:22 -0700

    SiddharthaThirdCommit

--- SourceCode/clickthrough.py
+++ SourceCode/clickthrough.py
@@ -1,6 +1,10 @@
-import sklearn
+
 import pandas as pd
 
-csv_file = "E:\\INF 550 Dataset\\train.csv"
+csv_file = "/home/squirrel/Documents/INF 550 Dataset/train.000"
 df = pd.read_csv(csv_file, index_col=0)
+hdf = pd.HDFStore("/home/squirrel/Documents/INF 550 Dataset/small.h5")
+hdf['df'] = df
+hdf.close()
+df.to_pickle("/home/squirrel/Documents/INF 550 Dataset/pickle")
 print df
