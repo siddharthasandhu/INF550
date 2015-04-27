e2119f1989c26c73bf4ae56999d7f9974d66b9bc
Siddhartha Singh Sandhu <sandhus@usc.edu>
2015-04-27 16:46:45 -0700

    Commit bash

--- SourceCode/vowpal.sh
+++ SourceCode/vowpal.sh
@@ -1,4 +1,8 @@
 #All 23
 
-vazu.model.vw --loss_function=logistic --hash strings -b 26 --learning_rate=0.6 --compressed --ftrl --ftrl_alpha=0.1 --ftrl_beta=1.0 --nn=10
+vw train.vw -f avazu.model.vw --loss_function=logistic --hash strings -b 26 --learning_rate=0.6 --compressed --ftrl --ftrl_alpha=0.1 --ftrl_beta=1.0 --nn=10
+vw test.vw -t -i avazu.model.vw -p avazu.preds.txt
+
+#All 23
+vw train.vw -f avazu.model.vw --loss_function logistic
 vw test.vw -t -i avazu.model.vw -p avazu.preds.txt
