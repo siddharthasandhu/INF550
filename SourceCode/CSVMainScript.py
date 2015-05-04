9b7ab15b9bf3d2b6c13ea26f58bfc4a2d4e651b1
Siddhartha Singh Sandhu <sandhus@usc.edu>
2015-05-03 21:01:52 -0700

    final commit 0.2

--- SourceCode/CSVMainScript.py
+++ SourceCode/CSVMainScript.py
@@ -1,7 +1,8 @@
 # -*- coding: UTF-8 -*-
 
 ########################################################
-# __Author__: Siddhartha            #
+# __Author__: Sabine Crevoisier and Fraser Campbell            #
+# Kaggle competition "Display Advertising Challenge":  #
 # https://www.kaggle.com/c/avazu-ctr-prediction #
 # Credit: Triskelion <info@mlwave.com>          #
 #  and Xueer Chen for providing the starter code for this competition, which we hereby modify.
@@ -12,6 +13,13 @@ from csv import DictReader
 
 def csv_to_vw(loc_csv, loc_output, train=True):
 
+  featuresToRemove = []
+  import csv
+  with open('featureRemove.csv', 'rb') as f:
+      reader = csv.reader(f)
+      for row in reader:
+          featuresToRemove.extend(row)
+  featuresToRemove = set(featuresToRemove)
 
   """
   Munges a CSV file (loc_csv) to a VW file (loc_output). Set "train"
@@ -20,10 +28,11 @@ def csv_to_vw(loc_csv, loc_output, train=True):
   """
   start = datetime.now()
   print("\nTurning %s into %s. Is_train_set? %s"%(loc_csv,loc_output,train))
-
+  
   with open(loc_output,"wb") as outfile:
     for e, row in enumerate( DictReader(open(loc_csv)) ):
-      #Creating the features
+	
+	  #Creating the features
       numerical_features = ""
       categorical_features = ""
       for k,v in row.items():
@@ -31,29 +40,30 @@ def csv_to_vw(loc_csv, loc_output, train=True):
           dt = datetime.strptime(v[4:6] + "/" + v[2:4] + "/" + v[0:2] + " " + v[6:8], "%d/%m/%y %H")
           # create a hour of the day feature, a day of the week feature, a month of the year feature
           categorical_features += "hour_" + str(v[6:8]) + " day_" + str(dt.weekday()) + " "
-        if k not in ["id","click"]:
+        if k not in ["id","click", "hour", "device_id", "device_ip"]:
           if len(str(v)) > 0 and str(v) != "d41d8cd9": #this removes the null values
+            if (k + "_" + v) not in featuresToRemove:
               categorical_features += " %s_%s " % (k,v)
             # else:
             #   print "I was not included :( and I am: " + (k + "_" + v)
-
-	  #Creating the labels
+			  
+	  #Creating the labels		  
       if train: #we care about labels
         if row['click'] == "1":
           label = 1
         else:
           label = -1 #we set negative label to -1
         outfile.write( "%s '%s |i%s |c%s\n" % (label,row['id'],numerical_features,categorical_features) )
-
+		
       else: #we dont care about labels
         outfile.write( "1 '%s |i%s |c%s\n" % (row['id'],numerical_features,categorical_features) )
-
+      
 	  #Reporting progress
       if e % 100000 == 0:
         print("%s\t%s"%(e, str(datetime.now() - start)))
 
   print("\n %s Task execution time:\n\t%s"%(e, str(datetime.now() - start)))
 
-
-csv_to_vw("/home/squirrel/Documents/INF 550 Dataset/train.csv", "/home/squirrel/Documents/INF 550 Dataset/train.second.vw",train=True)
-csv_to_vw("/home/squirrel/Documents/INF 550 Dataset/test.csv", "/home/squirrel/Documents/INF 550 Dataset/test.second.vw",train=False)
+csv_to_vw("/home/squirrel/Documents/INF 550 Dataset/train.csv", "/home/squirrel/Documents/INF 550 Dataset/model/reducedTrain.vw",train=True)
+csv_to_vw("/home/squirrel/Documents/INF 550 Dataset/test.csv", "/home/squirrel/Documents/INF 550 Dataset/model/reducedtest.vw",train=False)
+csv_to_vw("/home/squirrel/Documents/INF 550 Dataset/test.000", "/home/squirrel/Documents/INF 550 Dataset/model/reducedtest.vw",train=False)
--- SourceCode/vowpalwabbit/csvtovw.py
+++ SourceCode/vowpalwabbit/csvtovw.py
@@ -1,7 +1,7 @@
 # -*- coding: UTF-8 -*-
 
 ########################################################
-# __Author__: Siddhartha Sandhu <sandhus@usc.edu>             #
+# __Author__: Xueer Chen <snowwing922@gmail.com>             #
 # Kaggle competition "Display Advertising Challenge":  #
 # https://www.kaggle.com/c/avazu-ctr-prediction #
 # Credit: Triskelion <info@mlwave.com>            #
@@ -9,6 +9,7 @@
 
 from datetime import datetime
 from csv import DictReader
+import sys
 
 def csv_to_vw(loc_csv, loc_output, train=True):
   """
@@ -18,10 +19,10 @@ def csv_to_vw(loc_csv, loc_output, train=True):
   """
   start = datetime.now()
   print("\nTurning %s into %s. Is_train_set? %s"%(loc_csv,loc_output,train))
-
+  
   with open(loc_output,"wb") as outfile:
-    for e, row in enumerate(DictReader(open(loc_csv))):
-
+    for e, row in enumerate( DictReader(open(loc_csv)) ):
+	
 	  #Creating the features
       numerical_features = ""
       categorical_features = ""
@@ -29,23 +30,41 @@ def csv_to_vw(loc_csv, loc_output, train=True):
         if k not in ["id","click"]:
           if len(str(v)) > 0:
            categorical_features += " %s" % v
-
-	  #Creating the labels
+			  
+	  #Creating the labels		  
       if train: #we care about labels
         if row['click'] == "1":
           label = 1
         else:
           label = -1 #we set negative label to -1
         outfile.write( "%s '%s |i%s |c%s\n" % (label,row['id'],numerical_features,categorical_features) )
-
+		
       else: #we dont care about labels
         outfile.write( "1 '%s |i%s |c%s\n" % (row['id'],numerical_features,categorical_features) )
-
+      
 	  #Reporting progress
       if e % 100000 == 0:
         print("%s\t%s"%(e, str(datetime.now() - start)))
 
   print("\n %s Task execution time:\n\t%s"%(e, str(datetime.now() - start)))
 
-csv_to_vw("/home/squirrel/Documents/INF 550 Dataset/train.000", "/home/squirrel/Documents/INF 550 Dataset/train000.vw",train=True)
-#csv_to_vw("/home/squirrel/Documents/INF 550 Dataset/test.csv", "/home/squirrel/Documents/INF 550 Dataset/test.vw",train=False)
+def main():
+    return 0
+  
+if __name__ == '__main__':
+  # main should return 0 for success, something else (usually 1) for error.
+    csv_to_vw("/home/squirrel/Documents/INF 550 Dataset/train.csv",
+          "/home/squirrel/Documents/INF 550 Dataset/model/trainOriginal.vw", train=True)
+    csv_to_vw("/home/squirrel/Documents/INF 550 Dataset/test.csv",
+          "/home/squirrel/Documents/INF 550 Dataset/model/testOriginal.vw", train=False)
+    csv_to_vw("/home/squirrel/Documents/INF 550 Dataset/train.000",
+          "/home/squirrel/Documents/INF 550 Dataset/model/trainProbs.vw", train=True)
+    sys.exit(main())
+
+
+#csv_to_vw("/home/wacax/Wacax/Kaggle/Avazu-CTR-Prediction/AvazuCTR/Data/testProbs.csv",
+#          "/home/wacax/Wacax/Kaggle/Avazu-CTR-Prediction/AvazuCTR/Data/vw/testProbs.vw", train=False)
+#csv_to_vw("/home/wacax/Wacax/Kaggle/Avazu-CTR-Prediction/AvazuCTR/Data/ProbsTfidf.csv",
+#          "/home/wacax/Wacax/Kaggle/Avazu-CTR-Prediction/AvazuCTR/Data/vw/ProbsTfidf.vw", train=True)
+#csv_to_vw("/home/wacax/Wacax/Kaggle/Avazu-CTR-Prediction/AvazuCTR/Data/testProbsTfidf.csv",
+#          "/home/wacax/Wacax/Kaggle/Avazu-CTR-Prediction/AvazuCTR/Data/vw/testProbsTfidf.vw", train=False)
